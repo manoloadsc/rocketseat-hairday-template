f767199c1b36f4e956da6516d168e3c8539fbb63
Emanuel Couto <contato.ecouto@gmail.com>
2025-07-15 05:39:00 -0300

    configuracao concluida

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// CSS
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
+
+// Configuração do DayJS
+import "./libs/dayjs.js";
+
+import dayjs from "dayjs";
+
+console.log(dayjs().format("DD/MM/YYYY"));
